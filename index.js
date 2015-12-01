52b64001510e0efe0d8ea86f6d8fedb23900b73e
Maximiliano Korp <maxkorp@8bytealchemy.com>
2015-12-01 16:48:05 -0700

    runnable via cli

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 #! /usr/bin/env node
 var cp = require('child_process');
-
-module.exports = function detectNodeWebkit(npgCommands, cb) {
-  var cmd = 'node';
-  var args = ['node_modules/.bin/node-pre-gyp'].concat(npgCommands);
-
-  [runtime, arch, target, disturl].forEach(function(config) {
-    var val = process.env['npm_config_' + config];
-    if (val) {
-      args.push('--' + config + '=' + val);
-    }
-  });
-
-  var proc = cp.spawn(cmd, args);
-  proc.stdout.pipe(process.stdout);
-  proc.stderr.pipe(process.stderr);
-  proc.on('exit', function(code) {
-    if (cb) {
-      cb(code);
-    }
-  });
+var npgCommands = Array.prototype.slice.call(process.argv, 2);
+if (!npgCommands.length) {
+  console.error('no commands specified');
+  process.exit(0);
 }
+
+var cmd = 'node';
+var args = ['node_modules/.bin/node-pre-gyp'].concat(npgCommands);
+
+['runtime', 'arch', 'target', 'disturl'].forEach(function(config) {
+  var val = process.env['npm_config_' + config];
+  if (val) {
+    args.push('--' + config + '=' + val);
+  }
+});
+
+var proc = cp.spawn(cmd, args);
+proc.stdout.pipe(process.stdout);
+proc.stderr.pipe(process.stderr);
+proc.on('exit', function(code) {
+  process.exit(code);
+});
